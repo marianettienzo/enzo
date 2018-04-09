d969792a59a679c5f1c08bfbad437d8f2716efa1
marianettienzo <35968819+marianettienzo@users.noreply.github.com>
2018-04-09 11:02:52 -0300

    NAVBAR ACTUALIZADO

--- navbar.php
+++ navbar.php
@@ -1,27 +1,35 @@
-<html>
 
- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
    <span class="navbar-toggler-icon"></span>
  </button>
 
  <div class="collapse navbar-collapse" id="navbarSupportedContent">
    <ul class="navbar-nav mr-auto">
-     <li class="nav-item active">
+
+ <?php if (!isset($_SESSION['usuario'])) { ?>
+    <li class="nav-item active">
        <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
      </li>
-     <li class="nav-item">
+<?php } ?>
+   
+<?php if ($_SESSION['usuario']) { ?>
+    <li class="nav-item">
        <a class="nav-link" href="formulario.php">Formulario</a>
      </li>
-     <li class="nav-item">
+<?php } ?>
+<?php if ($_SESSION['usuario']) { ?>
+    <li class="nav-item">
        <a class="nav-link disabled" href="tabla.php">Tabla</a>
      </li>
-     <li class="nav-item">
+<?php } ?>
+<?php if ($_SESSION['usuario']) { ?>
+    <li class="nav-item">
        <a class="nav-link" href="salir.php">Logout</a>
      </li>
+<?php } ?>     
    </ul>
  </div>
 </nav>
 
 
-</html>
